06c3fdf0e6c2cb7b19e254d6ab595424d01cf00e
Brad <brjrafferty@gmail.com>
2022-04-02 13:18:39 -0500

    Improve file pathing
    Update file pathing for the current relative path locations, and also allow user to select the particular video file they want to process.

--- src/main.m
+++ src/main.m
@@ -5,11 +5,11 @@ wd = pwd;
 pause(3)
 
 %% Add paths
-addpath([wd '\code\plot\'])
-addpath([wd '\code\process_data\'])
-addpath([wd '\code\process_image\'])
-addpath([wd '\code\read_write\'])
-addpath([wd '\code\edit_vid\'])
+addpath('.\plot\')
+addpath('.\process_data\')
+addpath('.\process_image\')
+addpath('.\read_write\')
+addpath('.\process_vid\')
 
 %% User inputs
 run_num = 3;
@@ -37,18 +37,18 @@ end_frame = 1295;
 % write_vid(frames_raw, 'test.mp4', 'MPEG-4')
 
 %% Process frames
-filename_out_vid_gray = [wd '\data\' run_str '\processed\videos\' ...
+filename_out_vid_gray = ['..\data\' run_str '\processed\videos\' ...
     run_str '_vid_gray'];
-filename_out_vid_bw = [wd '\data\' run_str '\processed\videos\' ...
+filename_out_vid_bw = ['..\data\' run_str '\processed\videos\' ...
     run_str '_vid_gray_bw'];
-dir_processed_frames = [wd '\data\' run_str '\processed\images\' run_str];
+dir_processed_frames = ['..\data\' run_str '\processed\images\' run_str];
 
 [frames_comp_gray, frames_bw, vid_obj_out_gray, vid_obj_out_gray_bw] =...
     process_img(frames_raw, filename_out_vid_gray, filename_out_vid_bw, ...
     dir_processed_frames);
 
 %% Crop, trim, and center video
-vid_filepath_cropped = [dir_vids run_str '_trimmed_cropped.mp4'];
+vid_filepath_cropped = [vid_dir_name run_str '_trimmed_cropped.mp4'];
 [vid_obj_bw_cr, frames_bw_cr, o_coords, cm_per_pixel] = ...
     crop_center_vid(frames_bw, vid_filepath_cropped, rot_angle,...
                     crop_side_length_cm, dia_drain, frame_ref);
@@ -56,38 +56,38 @@ vid_filepath_cropped = [dir_vids run_str '_trimmed_cropped.mp4'];
 %% Analyze the binary frames for the characteristics of the core
 [core_props_struct, core_props_tbl] = ...
     detect_core(frames_bw_cr, o_coords, cm_per_pixel, run_num);
-save([wd '\data\' run_str '\processed\core_properties\' run_str ...
+save(['..\data\' run_str '\processed\core_properties\' run_str ...
     '_tbl.mat'], 'core_props_tbl');
-save([wd '\data\' run_str '\processed\core_properties\' run_str ...
+save(['..\data\' run_str '\processed\core_properties\' run_str ...
     '_struct.mat'], 'core_props_struct');
 
 %% Plot
-plot_core_props(run002, 30, .9);
-
-%% Mesh
-
-%% Concatenate the data
-tbl_all = [run002; run003; run004; run005];
-
-%% Plot over multiple realizations
-plot_core_props_all(tbl_all, 30);
+% plot_core_props(run002, 30, .9);
+% 
+% %% Mesh
+% 
+% %% Concatenate the data
+% tbl_all = [run002; run003; run004; run005];
+% 
+% %% Plot over multiple realizations
+% plot_core_props_all(tbl_all, 30);
 
 %% Histogram
-figure('Color', 'w')
-    subplot(1,2,1)
-        histogram(tbl_all.x_c, 'Normalization', 'pdf')
-        xlabel('$x_c$, cm')
-        ylabel('PDF')
-        xlim([-8 8])
-        ylim([0 1.3])
-        set(gca, 'fontsize', 16)
-    subplot(1,2,2)
-        histogram(tbl_all.y_c, 'Normalization', 'pdf')
-        xlabel('$y_c$, cm')
-        ylabel('PDF')
-        xlim([-8 8])
-        ylim([0 1.3])
-        set(gca, 'fontsize', 16)
+% figure('Color', 'w')
+%     subplot(1,2,1)
+%         histogram(tbl_all.x_c, 'Normalization', 'pdf')
+%         xlabel('$x_c$, cm')
+%         ylabel('PDF')
+%         xlim([-8 8])
+%         ylim([0 1.3])
+%         set(gca, 'fontsize', 16)
+%     subplot(1,2,2)
+%         histogram(tbl_all.y_c, 'Normalization', 'pdf')
+%         xlabel('$y_c$, cm')
+%         ylabel('PDF')
+%         xlim([-8 8])
+%         ylim([0 1.3])
+%         set(gca, 'fontsize', 16)
         
 %% Plot over time
 

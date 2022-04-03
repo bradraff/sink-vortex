742b0428ffabc05972bd72998018144ad2f9b4a0
Brad <brjrafferty@gmail.com>
2022-04-03 18:30:58 -0500

    Add annotations to user-interactive images
    Include titles on plots in order to guide user through process for zooming, cropping, and adding reference points to images.

--- src/plot/convert_px_to_cm.m
+++ src/plot/convert_px_to_cm.m
@@ -1,5 +1,5 @@
 function cm_per_px = convert_px_to_cm(frame, lengthscale, coords_center)
-%%convert_px_to_mm
+%%convert_px_to_cm
 
 x_c = coords_center(1);
 y_c = coords_center(2);
@@ -8,18 +8,23 @@ figure
     imshow(frame)
     hold on
     plot(x_c, y_c, 'g+')
+
     % Allow user to set zoom
+    title('Drag-to-zoom around drain perimeter, then press any key')
     zoom on;
     pause()
     zoom off;
+
     % Prompt user to select opposite ends of the drain to determine its
     % diameter
+    title('Select two opposite points on the drain perimeter, separated by one diameter')
     [x1, y1] = ginput(1);
     plot(x1, y1, 'r+')
     [x2, y2] = ginput(1);
     plot(x2, y2, 'r+')
 close
 
+% Calculate the distance in pixels and, subsequently, the conversion to eu
 distance_pixels = sqrt( (x1 - x2)^2 + (y1 - y2)^2 );
 cm_per_px = lengthscale / distance_pixels;
 
--- src/plot/select_drain_center.m
+++ src/plot/select_drain_center.m
@@ -3,19 +3,24 @@ function [x_c, y_c] = select_drain_center(frame)
 
 cnt = 1;
 figure
-    imshow(frame)
-    hold on
+imshow(frame)
+hold on
 while 1
+
     % Allow user to set zoom
     if cnt == 1
+        title('Drag-to-zoom around drain perimeter, press any key to continue')
         zoom on;
         pause()
         zoom off;
     end
+
     % Prompt user to select center of drain, using a crosshair
+    title('Left-click center of the drain, right click when finished')
     [x_c, y_c] = ginput(1); 
     selection = get(gcf, 'SelectionType'); 
-    if strcmpi(selection, 'alt') % check if the user input was a right click
+    % Check if the user input was a right click
+    if strcmpi(selection, 'alt') 
         x_c = x_temp;
         y_c = y_temp;
         break
@@ -23,7 +28,8 @@ while 1
     plot(x_c, y_c, 'g+')
     x_temp = x_c;
     y_temp = y_c;
-    cnt = cnt+1;
+    cnt = cnt + 1;
+
 end
 close
 fprintf('Center location is [%f %f]\n', x_c, y_c)
